00f5ff1d6dfc8bd9d119b2cf4e67b306f0d7e8da
woobba94 <jwj3199@naver.com>
2021-12-11 21:12:22 +0900

    :white_check_mark: Update: initBtn
    1. Remove startTimer() : timer.js > line: 37 에 이미 정의됨
    2. Visible control -> .remove()
    3. Add next() : just test function

--- js/test-init.js
+++ js/test-init.js
@@ -1,10 +1,31 @@
+const questionBox = document.querySelector(".question-box");
+
+const initButton = document.querySelector(".btn-init");
+
+// 시작버튼 이벤트 리스너
+initButton.addEventListener("click", testInit);
+
+// 다음 버튼 이벤트 리스너 (임시)
 document
-    .querySelector("#btn-init")
-    .addEventListener("click", testInit);
+    .querySelector(".btn-next2")
+    .addEventListener("click", next);
 
 function testInit() {
-    // timerStart();
+    // 타이머 시작 timer.js > line: 37 에 이미 정의됨
+    // startTimer();
+
+    // .question-box 렌더링
     toggleToVisible();
+    initButton.remove();
+
+    // 첫 문제 .question-box 에 출력
+    // 데이터 로드 > 출력
+    fetch("/data/data.json")
+        .then((response) => response.json())
+        .then(
+            (data) =>
+                (questionBox.innerHTML = getQuestion(data))
+        );
 }
 
 function toggleToVisible() {
@@ -14,4 +35,14 @@ function toggleToVisible() {
     document
         .querySelector(".visible")
         .classList.replace("visible", "invisible");
+}
+
+function next() {
+    // 데이터 로드 > 출력
+    fetch("/data/data.json")
+        .then((response) => response.json())
+        .then(
+            (data) =>
+                (questionBox.innerHTML = getQuestion(data))
+        );
 }
