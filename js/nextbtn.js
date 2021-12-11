400954d89a7a1a798e26c389f5df5131618d5ea1
useon <74897720+useon@users.noreply.github.com>
2021-12-12 02:05:33 +0900

    :sparkles: Add: nextBtn.js

---
+++ js/nextbtn.js
@@ -0,0 +1,11 @@
+const nextBtn = document.querySelector(".btn-next")
+
+nextBtn.addEventListener("click", next);
+
+function next() {
+    fetch("/data/data.json")
+        .then((response) => response.json())
+        .then(
+            (data) =>
+                (questionBox.innerHTML = getQuestion(data))
+        );
