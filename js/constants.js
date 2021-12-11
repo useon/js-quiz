ffe451e5e82265c3196a46af7e4a04e305220af2
ImJunHong <tensai3355@gmail.com>
2021-12-11 19:34:28 +0900

    :sparkles: Add: constants.js

---
+++ js/constants.js
@@ -0,0 +1,5 @@
+const NUM = [ 0, 1, 2, 3, 4, 5 ];
+const DIVISOR = [ 0, 1, 2, 4, 5 ];
+const ALPHABET = [ 'a', 'b', 'c', 'd', 'e' ];
+const FALSY = [ '', '0', 'false', '[]', '{}', 'null', 'undefined' ];
+const BOOLEAN = [ 'true', 'false' ];
