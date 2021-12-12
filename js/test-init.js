62ee2c55d1eb42e87cc058cc89c2fd2e041b169c
jiseung-kang <ill0o0lli@ajou.ac.kr>
2021-12-12 17:42:51 +0900

    :sparkles: Add : 문제 및 유저 정답 데이터 저장 기능 추가

--- js/test-init.js
+++ js/test-init.js
@@ -42,15 +42,23 @@ function next() {
   fetch("/data/data.json")
     .then((response) => response.json())
     .then((data) => {
+      // 문제 데이터 저장 및 출력
       let question = getQuestion(data);
       questionBox.innerHTML = question;
       questions.push(question);
     });
-
+  // 사용자 입력 데이터 저장
   inpAnswers.push(inpAnswer.value);
 
-  // test
-  console.log(questions, inpAnswers);
-
-  getData(questions, inpAnswers);
+  // test code
+  // console.log(questions, inpAnswers);
 }
+
+// 최종 제출시 로컬스토리지에 데이터 저장 (버튼 클릭으로 테스트 -> 타이머 종료 구현시 수정)
+const goBtn = document.querySelector(".go-result");
+goBtn.addEventListener("click", function () {
+  localStorage.setItem("questions", JSON.stringify(questions));
+  localStorage.setItem("inpAnswers", JSON.stringify(inpAnswers));
+  // 테스트용 페이지 전환
+  location.href = "../page/result.html";
+});
