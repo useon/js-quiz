f16e372d7a5423cf4bcfe4db2c8f5afad205ff86
woobba94 <jwj3199@naver.com>
2021-12-13 10:32:54 +0900

    ✅ Update: code format
    1. Quotes: double  -> single
    2. Tab width: 4 -> 2

--- js/nextbtn.js
+++ js/nextbtn.js
@@ -1,9 +1,9 @@
-const nextBtn = document.querySelector(".btn-next");
+const nextBtn = document.querySelector('.btn-next');
 
-nextBtn.addEventListener("click", next);
+nextBtn.addEventListener('click', next);
 
 function next() {
-  fetch("/data/data.json")
+  fetch('/data/data.json')
     .then((response) => response.json())
     .then((data) => (questionBox.innerHTML = getQuestion(data)));
 }
--- js/test-init.js
+++ js/test-init.js
@@ -1,15 +1,15 @@
 var questions = [];
 var inpAnswers = [];
 
-const questionBox = document.querySelector(".question-box");
-const initButton = document.querySelector(".btn-init");
-let inpAnswer = document.querySelector(".input-box");
+const questionBox = document.querySelector('.question-box');
+const initButton = document.querySelector('.btn-init');
+let inpAnswer = document.querySelector('.input-box');
 
 // 시작버튼 이벤트 리스너
-initButton.addEventListener("click", testInit);
+initButton.addEventListener('click', testInit);
 
 // 다음 버튼 이벤트 리스너 (임시)
-document.querySelector(".btn-next2").addEventListener("click", next);
+document.querySelector('.btn-next2').addEventListener('click', next);
 
 function testInit() {
   // 타이머 시작 timer.js > line: 37 에 이미 정의됨
@@ -21,7 +21,7 @@ function testInit() {
 
   // 첫 문제 .question-box 에 출력
   // 데이터 로드 > 출력
-  fetch("/data/data.json")
+  fetch('/data/data.json')
     .then((response) => response.json())
     .then((data) => {
       let question = getQuestion(data);
@@ -31,15 +31,13 @@ function testInit() {
 }
 
 function toggleToVisible() {
-  document
-    .querySelector(".invisible")
-    .classList.replace("invisible", "visible");
-  document.querySelector(".visible").classList.replace("visible", "invisible");
+  document.querySelector('.invisible').classList.replace('invisible', 'visible');
+  document.querySelector('.visible').classList.replace('visible', 'invisible');
 }
 
 function next() {
   // 데이터 로드 > 출력
-  fetch("/data/data.json")
+  fetch('/data/data.json')
     .then((response) => response.json())
     .then((data) => {
       // 문제 데이터 저장 및 출력
@@ -55,10 +53,10 @@ function next() {
 }
 
 // 최종 제출시 로컬스토리지에 데이터 저장 (버튼 클릭으로 테스트 -> 타이머 종료 구현시 수정)
-const goBtn = document.querySelector(".go-result");
-goBtn.addEventListener("click", function () {
-  localStorage.setItem("questions", JSON.stringify(questions));
-  localStorage.setItem("inpAnswers", JSON.stringify(inpAnswers));
+const goBtn = document.querySelector('.go-result');
+goBtn.addEventListener('click', function () {
+  localStorage.setItem('questions', JSON.stringify(questions));
+  localStorage.setItem('inpAnswers', JSON.stringify(inpAnswers));
   // 테스트용 페이지 전환
-  location.href = "../page/result.html";
+  location.href = '../page/result.html';
 });
