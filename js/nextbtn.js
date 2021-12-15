5942c32f87ebce83263e2f6a700d468cd4e6ac1b
jiseung-kang <ill0o0lli@ajou.ac.kr>
2021-12-15 21:57:29 +0900

    ✨ Add array of right answers

--- js/nextbtn.js
+++ js/nextbtn.js
@@ -1,95 +1,100 @@
-var questions = [];
-var inpAnswers = [];
+const questions = []
+const inpAnswers = []
+const rightAnswers = []
 // timer.js 에 이미 선언됨
 // const nextBtn = document.querySelector('.btn-next');
-let inpAnswer = document.querySelector('.input-box');
-let okBtn = document.querySelector('.btn-ok');
-let rightAnswerBox = document.querySelector('.right-answer-box');
-let rightAnswerOutput = document.querySelector('.right-answer');
+let inpAnswer = document.querySelector('.input-box')
+let okBtn = document.querySelector('.btn-ok')
+let rightAnswerBox = document.querySelector('.right-answer-box')
+let rightAnswerOutput = document.querySelector('.right-answer')
 
-nextBtn.addEventListener('click', successCheck);
-okBtn.addEventListener('click', next);
+nextBtn.addEventListener('click', successCheck)
+okBtn.addEventListener('click', next)
 // input 에 keydown 이벤트 연결
-inpAnswer.addEventListener('keydown', enterKeydown);
+inpAnswer.addEventListener('keydown', enterKeydown)
 
 // 엔터키 입력 시 문제 제출
 function enterKeydown(e) {
-  if (e.key == 'Enter') {
-    successCheck();
-  }
+	if (e.key == 'Enter') {
+		successCheck()
+	}
 }
 
 // 틀렸을 때 정답 띄우기
 function showRightAnswer() {
-  // 타이머 일시정지
-  // timerPause();
+	// 타이머 일시정지
+	// timerPause();
 
-  // 버튼 비활성화
-  nextBtn.setAttribute('disabled', true);
-  // 정답박스 열기
-  rightAnswerBox.style.height = '100px';
+	// 버튼 비활성화
+	nextBtn.setAttribute('disabled', true)
+	// 정답박스 열기
+	rightAnswerBox.style.height = '100px'
 }
 
 function successCheck() {
-  // 답란이 비어있다면 실행 X
-  if (checkEmpty()) return;
-  // 정답 체크 분기 (미완성)
-  // 첫번째 분기 : 정답일때
-  // else : 오답일때 정답보여주기
-  if (isAnswer(questionBox.innerText, inpAnswer.value)) {
-    next();
-  } else {
-    showRightAnswer();
-  }
+	// 답란이 비어있다면 실행 X
+	if (checkEmpty()) return
+	// 정답 체크 분기 (미완성)
+	// 첫번째 분기 : 정답일때
+	// else : 오답일때 정답보여주기
+	if (isAnswer(questionBox.innerText, inpAnswer.value)) {
+		next()
+	} else {
+		showRightAnswer()
+	}
 }
 
 function next() {
-  // 버튼 활성화
-  nextBtn.removeAttribute('disabled');
-  // 정답박스 접기
-  rightAnswerBox.style.height = null;
+	// 버튼 활성화
+	nextBtn.removeAttribute('disabled')
+	// 정답박스 접기
+	rightAnswerBox.style.height = null
 
-  // 다음문제 출력
-  showQuestion();
-  // 사용자 입력 데이터 저장
-  inpAnswers.push(inpAnswer.value);
-  // 답란 비우기
-  clearInputBox();
+	// 다음문제 출력
+	showQuestion()
+	// 사용자 입력 데이터 저장
+	inpAnswers.push(inpAnswer.value)
+	// 답란 비우기
+	clearInputBox()
 }
+
+console.log(rightAnswers)
 
 // 문제 출력
 function showQuestion() {
-  fetch('/data/data.json')
-    .then((response) => response.json())
-    .then((data) => {
-      // 문제 데이터 저장 및 출력
-      let question = getQuestion(data);
-      questionBox.innerHTML = question;
-      questions.push(question);
-
-      // 정답박스 준비 (테스트용)
-      rightAnswerOutput.innerHTML = `정답 : ${getAnswer(question)}`;
-    });
+	fetch('/data/data.json')
+		.then((response) => response.json())
+		.then((data) => {
+			// 문제 데이터 저장 및 출력
+			let question = getQuestion(data)
+			questionBox.innerHTML = question
+			questions.push(question)
+			rightAnswers.push(getAnswer(question) + '')
+			console.log(rightAnswers)
+			// 정답박스 준비 (테스트용)
+			rightAnswerOutput.innerHTML = `정답 : ${getAnswer(question)}`
+		})
 }
 
 // 답란이 비어있는지 체크
 function checkEmpty() {
-  if (inpAnswer.value === '') {
-    inpAnswer.focus();
-    return true;
-  }
-  return false;
+	if (inpAnswer.value === '') {
+		inpAnswer.focus()
+		return true
+	}
+	return false
 }
 
 function clearInputBox() {
-  inpAnswer.value = '';
+	inpAnswer.value = ''
 }
 
 // 최종 제출시 로컬스토리지에 데이터 저장 (버튼 클릭으로 테스트 -> 타이머 종료 구현시 수정)
-const goBtn = document.querySelector('.go-result');
+const goBtn = document.querySelector('.go-result')
 goBtn.addEventListener('click', function () {
-  localStorage.setItem('questions', JSON.stringify(questions));
-  localStorage.setItem('inpAnswers', JSON.stringify(inpAnswers));
-  // 테스트용 페이지 전환
-  location.href = '../page/result.html';
-});
+	localStorage.setItem('questions', JSON.stringify(questions))
+	localStorage.setItem('inpAnswers', JSON.stringify(inpAnswers))
+	localStorage.setItem('rightAnswers', JSON.stringify(rightAnswers))
+	// 테스트용 페이지 전환
+	location.href = '../page/result.html'
+})
--- js/result.js
+++ js/result.js
@@ -1,28 +1,43 @@
-var right = 0;
-var wrong = 0;
+let right = 0
+let wrong = 0
 
-const resRight = document.querySelector('.right');
-const resWrong = document.querySelector('.wrong');
-const showBtn = document.querySelector('.showResult');
+const resRight = document.querySelector('.right')
+const resWrong = document.querySelector('.wrong')
+const showBtn = document.querySelector('.showResult')
+const myScore = document.querySelector('.score')
+const resTable = document.querySelector('tbody')
 
 // 로컬스토리지에서 데이터 불러오기
 function getData() {
-  questions = JSON.parse(localStorage.getItem('questions'));
-  inpAnswers = JSON.parse(localStorage.getItem('inpAnswers'));
+	questions = JSON.parse(localStorage.getItem('questions'))
+	inpAnswers = JSON.parse(localStorage.getItem('inpAnswers'))
+	rightAnswers = JSON.parse(localStorage.getItem('rightAnswers'))
 }
 
 // 결과
 showBtn.addEventListener('click', function showResult() {
-  getData();
-  console.log(questions, inpAnswers);
-  calResult();
-  resRight.innerHTML = '맞은 개수 : ' + right;
-  resWrong.innerHTML = '틀린 개수 : ' + wrong;
-});
+	getData()
+	console.log(questions, inpAnswers)
+	calResult()
+	resRight.innerHTML = '맞은 개수 : ' + right
+	resWrong.innerHTML = '틀린 개수 : ' + wrong
+	myScore.innerHTML = '내 점수 : ' + right + '/' + (right + wrong)
+})
 
-// 맞은 개수, 틀린 개수 계산
+// 맞은 개수, 틀린 개수 계산. 결과 테이블 출력
 function calResult() {
-  for (let i = 0; i < inpAnswers.length; i++) {
-    isAnswer(questions[i], inpAnswers[i]) ? (right += 1) : (wrong += 1);
-  }
+	for (let i = 0; i < inpAnswers.length; i++) {
+		isAnswer(questions[i], inpAnswers[i]) ? (right += 1) : (wrong += 1)
+		const q = document.createElement('td')
+		q.textContent = questions[i]
+		const a = document.createElement('td')
+		a.textContent = rightAnswers[i]
+		const inp = document.createElement('td')
+		inp.textContent = inpAnswers[i]
+		const datarow = document.createElement('tr')
+		datarow.appendChild(q)
+		datarow.appendChild(a)
+		datarow.appendChild(inp)
+		resTable.appendChild(datarow)
+	}
 }
