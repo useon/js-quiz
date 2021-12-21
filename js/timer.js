4fca426c6b2080d55d130c7be1a071fc7bf7864f
woobba94 <jwj3199@naver.com>
2021-12-21 14:19:38 +0900

    ✨Add: Bettery effect

--- js/timer.js
+++ js/timer.js
@@ -1,37 +1,59 @@
 const timer = document.querySelector('.timer');
-
+const bettery = document.querySelector('.bettery-area>i');
 // 제출버튼
 // const submitBtn = document.querySelector('.btn-submit');
 // 오답 확인버튼
 // const wrongConfirmBtn = document.querySelector('.btn-confirm');
 let interval;
+let betteryInterval;
 
+let betteryCount = 1;
+// 시간 기준 6000 : 1 min
+const MAX_TIME = 18000;
 // centisecond 단위로 입력하면 됩니다
-let TIME = 18000; // -> time limit 3분으로 설정
+let TIME = MAX_TIME; // -> time limit 3분으로 설정
 
 // 타이머 리셋 (남은 시간을 3분으로 설정)
 function resetTimer() {
-  TIME = 18000;
+  TIME = MAX_TIME;
 }
 
 // 타이머 시작 (리셋 및 시간줄이기)
 function startTimer() {
   resetTimer();
   interval = setInterval(updateTimer, 10);
+  betteryInterval = setInterval(updateBettery, 1000);
   // 10ms(10centisecond)마다 updateTimer 실행
 }
 
 // 타이머 일시정지
 function pauseTimer() {
   clearInterval(interval);
+  clearInterval(betteryInterval);
 }
 
 // 타이머 재시작
 function reStartTimer() {
   if (interval) {
     clearInterval(interval);
+    clearInterval(betteryInterval);
   }
   interval = setInterval(updateTimer, 10);
+  betteryInterval = setInterval(updateBettery, 1000);
+}
+
+function updateBettery() {
+  // 기준 시간에 대한 비율
+  let timeData = (TIME / MAX_TIME) * 100;
+  if (60 <= timeData && timeData < 80) {
+    bettery.classList.replace('fa-battery-full', 'fa-battery-three-quarters');
+  } else if (40 <= timeData) {
+    bettery.classList.replace('fa-battery-three-quarters', 'fa-battery-half');
+  } else if (20 <= timeData) {
+    bettery.classList.replace('fa-battery-half', 'fa-battery-quarter');
+  } else {
+    bettery.classList.replace('fa-battery-quarter', 'fa-battery-empty');
+  }
 }
 
 // 타이머 업데이트 (시간 html에 표시 및 남은시간 줄이기)
@@ -53,7 +75,3 @@ function updateTimer() {
     pauseTimer();
   }
 }
-
-// questionPage로 옮기겠습니당
-// startBtn.addEventListener('click', startTimer);
-// wrongConfirmBtn.addEventListener('click', startTimer);
